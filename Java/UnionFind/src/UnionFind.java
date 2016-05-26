e56113622419b3d9ab904ae77098b47ce2c9fa68
Toni Andreev <Toni Andreev>
2016-05-26 13:14:24 +0100

    Finalized the first version of the tester

--- Java/UnionFind/src/UnionFind.java
+++ Java/UnionFind/src/UnionFind.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class UnionFind {
 
@@ -18,8 +19,27 @@ public class UnionFind {
 	
 	// Tester code
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int N;
+		int p, q;
+		Scanner line_scanner = new Scanner(System.in);
+		UnionFind uf;
+		
+		N = Integer.parseInt(line_scanner.nextLine());
+		uf = new UnionFind(N);
+		
+		while(line_scanner.hasNextLine()){
+			String line = line_scanner.nextLine();
+			String[] split_line = line.split(" ");
+			p = Integer.parseInt(split_line[0]);
+			q = Integer.parseInt(split_line[1]);
+						
+			if(!uf.connected(p, q)){
+				uf.union(p, q);
+				System.out.println("" + p + " " + q);
+			}
+		}
+		
+		line_scanner.close();
 	}
 
 }
