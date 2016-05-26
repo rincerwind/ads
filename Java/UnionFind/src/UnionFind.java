95423cf31ff5fe33479c316f562329d573b79a42
Toni Andreev <Toni Andreev>
2016-05-26 12:44:58 +0100

    created union-find template

---
+++ Java/UnionFind/src/UnionFind.java
@@ -0,0 +1,25 @@
+
+public class UnionFind {
+
+	// Init. union-find data-struct with N objs. (indexed from 0 to N-1)
+	public UnionFind(int N){}
+	
+	// create a connection between p and q
+	public void union(int p, int q){}
+	
+	// check if there is a path between p and q (are they in the same component)
+	public boolean connected(int p, int q){return true;}
+	
+	// returns component identifier of p
+	public int find(int p){return 0;}
+	
+	// returns number of components
+	public int count(){return 0;}
+	
+	// Tester code
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
